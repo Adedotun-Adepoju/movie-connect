778db4189d246cb790db2a5f9ca75bf97bed5867
Adedotun-Adepoju <d.e.adepoju@gmail.com>
2024-02-10 13:49:21 +0100

    implement response helper

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto, SignUpDto } from './dto/auth.dto';
+import { ResponseHelper, ResponseInterface } from 'src/helper/response.helper';
 
 @Controller('auth')
 export class AuthController {
@@ -10,11 +11,21 @@ export class AuthController {
 
   @Post('/sign-up')
   async signUp(@Body() signUpDto: SignUpDto){
-    await this.authService.signUp(signUpDto)
+    try {
+      const res: ResponseInterface = await this.authService.signUp(signUpDto)
+      return ResponseHelper.successResponse(res.message, res.status_code, res.data)
+    } catch(error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
   }
 
   @Post('sign-in')
   async signIn(@Body() signInDto: SignInDto){
-    console.log({ signInDto })
+    try {
+      const res: ResponseInterface = await this.authService.signIn(signInDto)
+      return ResponseHelper.successResponse(res.message, res.status_code, res.data)
+    } catch(error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
   }
 }
