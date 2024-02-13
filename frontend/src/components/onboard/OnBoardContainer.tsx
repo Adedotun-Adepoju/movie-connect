31982bea35762c4b3815ccc633fc8c7533347e23
Christian Onoh <chibyk5000@gmail.com>
2024-02-13 10:37:03 +0100

    Add OnBoardContainer component

---
+++ frontend/src/components/onboard/OnBoardContainer.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { ReactNode } from "react";
+import onboardBg from "../../../public/images/onboard-bg.webp";
+
+interface Prop {
+  children: ReactNode;
+}
+
+const OnBoardContainer = ({ children }: Prop) => {
+  return (
+    <div className="mx-auto flex h-screen w-full max-w-6xl gap-16">
+      <div className="h-full w-1/2 hidden lg:flex  max-w-lg rounded-3xl bg-primary">
+        <Image
+          src={onboardBg}
+          alt="Cinema"
+          className="h-full rounded-3xl object-cover"
+        />
+      </div>
+      <div className="basis-1/2 flex-1 flex-col">{children}</div>
+    </div>
+  );
+};
+
+export default OnBoardContainer;
