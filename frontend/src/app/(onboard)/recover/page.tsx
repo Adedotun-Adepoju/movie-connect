7b722ccb2b64651e32cdb50b517c0def6d861063
Christian Onoh <chibyk5000@gmail.com>
2024-02-29 09:41:16 +0100

    Refactor to group login, recover password routes to use same layout

---
+++ frontend/src/app/(onboard)/recover/page.tsx
@@ -0,0 +1,13 @@
+import NewPasswordForm from "@/components/onboard/NewPasswordForm";
+import RecoverForm from "@/components/onboard/RecoverForm";
+
+const page = () => {
+  return (
+    <>
+      <RecoverForm />
+      {/* <NewPasswordForm /> */}
+    </>
+  );
+};
+
+export default page;
